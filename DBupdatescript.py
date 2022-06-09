145b8994e7ed58307b5722720b476847931c7c48
Robert Kwiatkowski <jakub.sienko@iandb.pl>
2022-06-09 20:27:36 +0200

    added custom exception for script arguments

--- DBupdatescript.py
+++ DBupdatescript.py
@@ -1,12 +1,22 @@
+import pymysql
+import sqlalchemy
 import customerrors as ce
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
 
 class Run:
     """Class responsible for carrying out the script
     
         Atributes:
-        ---
+
+        -----
         Methods:
         argschk -- checking correctness of passed arguments
+        setup   -- setups the currency table needed to 
+        update  -- updates currencies
+        export  -- exports data from database into .csv format
+        help    -- 
     """
     def __init__ (self, cmdargs):
         try:
@@ -14,17 +24,52 @@ class Run:
         except ce.ArgErr as err:
             print(str(err))
             return
-        for i in cmdargs:
-            print(i)
+        try:
+            if cmdargs[1] == "-s":
+                self.setup()
+            elif cmdargs[1] == "-u":
+                self.update()
+            elif cmdargs[1] == "-e":
+                self.export()
+            else:
+                self.help()
+        except sqlalchemy.exc.OperationalError as err:
+            print("Given database does not exist or is unreachable.")
     
     def argschk(self, cmdargs):
         if len(cmdargs) != 2:
             if len(cmdargs) < 2:
-                msg = 'Argument missing, please provide "-s" to modify exisitng database to fullfil requirements\n' \
-                    + '"-u" to collect current currency rates and "-e" to export data to .csv file.'
+                msg = 'Brakuje argumentu'
             else:
-                msg = 'Too many arguments provided, please provide only one.'
+                msg = 'Za dużo argumentów'
             raise ce.ArgErr(msg)
-        elif cmdargs[1] not in ["-e", "-u", "-s"]:
-            msg = ""
+        elif cmdargs[1] not in ["-e", "-u", "-s", "-h", "-help"]:
+            msg = 'Nieznany argument'
             raise ce.ArgErr(msg)
+
+    def setup(self):
+        Base = automap_base()
+        try:
+            engine = create_engine("mysql+pymysql://root:root@localhost/wakamakafon")
+        except pymysql.err.OperationalError as err:
+            print(str(err))
+        Base.prepare(engine, reflect=True)
+        session = Session(engine)
+        if not Base.classes.__contains__('product'):
+            ## TODO should raise error that database is
+            k = 1
+        else:
+            k =2
+        return
+
+    def update(self):
+        return
+    
+    def export(self):
+        return
+    
+    def help(self):
+        print("main.py opcje[-s, -u, -e]\njedna z opcji jest wymagana do działania skryptu\n \
+-s\t->\tmodyfikacja istniejącej bazy danych spełniającej wymagania by była w stanie przyjąć nowe waluty.\n \
+-u\t->\todświerzenie kursów walut pobranych z API NPB.\n \
+-e\t->\teksportowanie danych do pliku .csv.")
