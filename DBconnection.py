904ac048c7248252bd6fe2781c8281faf5713290
Robert Kwiatkowski <jakusienko@gmail.com>
2022-07-26 17:04:50 +0200

    added documentation and comforted class to pep8

--- DBconnection.py
+++ DBconnection.py
@@ -2,24 +2,32 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
+
 class dbconn:
-    """Class responsible for database connection
+    """ Class responsible for database connection
 
-        Atributes:
-        engine  -- engine responsible for code to database communication 
-        base    -- variable holding the schema of automapped database
-        session -- variable responsible for passing querries
+        Atributes:\n
+        CONN_STR    -- variable holding connection adress for database\n
+        engine      -- engine responsible for code to database communication\n
+        base        -- variable holding the schema of automapped database\n
+        session     -- variable responsible for passing querries\n
 
-        Methods:
+        Methods:\n
         refresh -- refreshes automaping of the database
     """
+    CONN_STR = "mysql+pymysql://root:root@localhost/mydb"
+
     def __init__(self):
-        self.engine = create_engine("mysql+pymysql://root:root@localhost/mydb")
+        self.engine = create_engine(self.CONN_STR)
         self.base = automap_base()
         self.base.prepare(self.engine, reflect=True)
         self.session = Session(self.engine)
-    
-    def refresh(self):
+
+    def refresh(self) -> None:
+        """ Resposnible for refreshing the map of the database, after the
+            currency table is created, to ensure access in the same
+            program run
+        """
         self.base = automap_base()
         self.base.prepare(self.engine, reflect=True)
         self.session = Session(self.engine)
