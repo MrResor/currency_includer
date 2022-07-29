ef48a8ec74cc27f69545987626a1878855e8d5d1
Robert Kwiatkowski <jakusienko@gmail.com>
2022-07-29 13:56:55 +0200

    added docstring and unified naming conventions

--- decorators.py
+++ decorators.py
@@ -4,6 +4,9 @@ from requests.exceptions import ConnectionError
 
 
 def db_errors(func):
+    """ Decorator responsible for handling database errors, such as not
+        existing database, incorrect credentials or database not accessible.
+    """
     def note(error: str) -> None:
         print(error)
         logging.error(error)
@@ -12,22 +15,25 @@ def db_errors(func):
         try:
             func(instance)
         except OperationalError as err:
-            call = {"1049": "Baza danych nie istnieje",
-                    "1045": "Niepoprawne dane logowania do bazy.",
-                    "2003": "Baza danych jest niedostępna"}
-            note(call[err.args[0].split("(")[2].split(",")[0]])
-            note("Program zakończony z kodem 2.")
+            call = {'1049': 'Baza danych nie istnieje',
+                    '1045': 'Niepoprawne dane logowania do bazy.',
+                    '2003': 'Baza danych jest niedostępna'}
+            note(call[err.args[0].split('(')[2].split(',')[0]])
+            note('Program zakończony z kodem 2.')
             quit(2)
     return wrapper_catch_db_errors
 
 
 def api_errors(func):
+    """ Decorator responsible for handling API errors, such as inaccessible
+        API endpoint.
+    """
     def wrapper_catch_api_errors(instance, flag):
         try:
             return func(instance, flag)
         except ConnectionError:
             print('Błąd połączenia z API banku.')
-            logging.error("Błąd połączenia z API banku.")
+            logging.error('Błąd połączenia z API banku.')
             if flag:
                 print('Nie udało się utworzyć tablicy "Currency".')
                 logging.error('Nie udało się utworzyć tablicy "Currency".')
@@ -38,13 +44,16 @@ def api_errors(func):
 
 
 def missing_table_errors(func):
+    """ Decorator responsible for handling table errors in database, such as
+        nonexisting tables.
+    """
     def wrapper_catch_missing_table_errors(instance, args):
         try:
             func(instance, args)
         except AttributeError as err:
             print(f'Baza źle skonfigurowana, brakuje tablicy "{str(err)}".')
-            logging.error(f'Baza źle skonfigurowana, brakuje tablicy\
-                "{str(err)}".')
+            logging.error(f'Baza źle skonfigurowana, brakuje tablicy'
+                          f'"{str(err)}".')
             print('Program zakończony z kodem 4.')
             logging.error('Program zakończony z kodem 4.')
             quit(4)
